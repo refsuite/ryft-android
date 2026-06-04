cf3510a40e53414bc1a724fad80bc198c4e5b535
Niyas Muhammed <niyas@refsuite.co.uk>
2026-06-04 17:19:41 +0100

    version: Bump SDK version to 2.0.2

--- ryft-core/src/main/java/com/ryftpay/android/core/model/Ryft.kt
+++ ryft-core/src/main/java/com/ryftpay/android/core/model/Ryft.kt
@@ -1,6 +1,6 @@
 package com.ryftpay.android.core.model
 
 internal object Ryft {
-    internal const val VERSION = "2.0.0"
+    internal const val VERSION = "2.0.2"
     internal const val USER_AGENT = "ryft-sdk-android/$VERSION"
 }
--- ryft-core/src/test/java/com/ryftpay/android/core/model/RyftTest.kt
+++ ryft-core/src/test/java/com/ryftpay/android/core/model/RyftTest.kt
@@ -7,11 +7,11 @@ internal class RyftTest {
 
     @Test
     fun `Version should have current version of sdk`() {
-        Ryft.VERSION shouldBeEqualTo "2.0.0"
+        Ryft.VERSION shouldBeEqualTo "2.0.2"
     }
 
     @Test
     fun `User agent should identify sdk with current version`() {
-        Ryft.USER_AGENT shouldBeEqualTo "ryft-sdk-android/2.0.0"
+        Ryft.USER_AGENT shouldBeEqualTo "ryft-sdk-android/2.0.2"
     }
 }
